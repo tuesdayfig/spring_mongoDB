6431dc879ecb52c82f8e99faedc37e5047cc7c7b
Makil <Makil@LAPTOP-3NNSEH4D>
2020-05-04 13:02:00 -0400

    new app

---
+++ src/test/java/guru/springframework/stfpetclinic/StfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.stfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
